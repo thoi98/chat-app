6e69ee36f1b8b7369029031a19877b7fcacf8978
thoi <thoithoi98@gmail.com>
2020-10-08 17:27:58 +0530

    Basic structure with users, and subscription to createUsers event.

---
+++ graphql/resolver/subscription.js
@@ -0,0 +1,11 @@
+const { pubsub } = require('../helper');
+
+module.exports = {
+    Subscription: {
+        user: {
+            subscribe(parent, args, ctx, info) {
+                return pubsub.asyncIterator('userTopic') //Topic
+            }
+        }
+    }
+}
---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const typeDefs = require('./graphql/schema/index');
+const resolvers = require('./graphQL/resolver/index');
+const { GraphQLServer } = require('graphql-yoga');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+mongoose.connect("mongodb+srv://" + 
+        "thoi" + ":"+
+        "WzXUcOdHt6D8rRFE" +
+        "@node-rest-shop.etpxr.mongodb.net/" +
+        "graphql-subscription-test" +
+        "?retryWrites=true&w=majority",{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+).then(() => console.log("\x1b[35m"," Database is connected..."))
+.catch(
+    (err) => console.error(err)
+);
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+});
+server.start(() => {
+    console.log('GraphQL Listening on port 4000');
+});
