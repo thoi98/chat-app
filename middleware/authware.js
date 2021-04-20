0819ec353442c8da21605d7933f8d8a2d79e8b0e
Thoi <thoithoi98@gmail.com>
2021-04-20 17:04:18 +0530

    authorization header not required.authware is changed to authorize all request

--- middleware/authware.js
+++ middleware/authware.js
@@ -3,19 +3,19 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
     const authHeader = req.get("Authorization");
     if (!authHeader) {
-        req.isAuth = false;
+        req.isAuth = true;
         return next();
     }
     const token = authHeader.split(" ")[1];
     if (!token || token === "") {
-        req.isAuth = false;
+        req.isAuth = true;
         return next();
     }
     let decodedtoken;
     try {
         decodedtoken = jwt.verify(token, process.env.SECRET_KEY);
     } catch (err) {
-        req.isAuth = false;
+        req.isAuth = true;
         return next();
     }
     req.isAuth = true;
